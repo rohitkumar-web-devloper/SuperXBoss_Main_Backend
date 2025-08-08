c7d38f4a85ac962e94c2dea600c6550d40cbc359
TusharGola28 <tushar.gola@ticketsque.com>
2025-08-08 20:45:25 +0530

    fix

--- src/Validation/brand.js
+++ src/Validation/brand.js
@@ -90,7 +90,7 @@ const updateBrandSchema = Joi.object({
       'string.base': `"brand_segment" must contain string ObjectIds`,
       'any.invalid': `"brand_segment" must contain valid ObjectIds`,
     }))
-    .required()
+    .optional()
     .messages({
       'array.base': `"brand_segment" must be an array`,
       'any.required': `"brand_segment" is required`,
